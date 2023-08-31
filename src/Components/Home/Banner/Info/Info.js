5779b62e6df88373bce27749f3d6ea70431c65dd
adnan shaikh <adnn4u@gmail.com>
2023-08-31 12:33:21 +0530

    31st first commit

--- src/Components/Home/Banner/Info/Info.js
+++ src/Components/Home/Banner/Info/Info.js
@@ -3,7 +3,21 @@ import "./Info.css"
 function Info() {
   return (
     <div className='Info'>
-      <div className="info-text">
+      <div className="info-text" >
+      <svg className='big-blue-box' width="294" height="283" viewBox="0 0 294 283" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="159.924" y="40.645" width="99" height="199.053" rx="29.5" transform="rotate(34.2964 159.924 40.645)" stroke="black"/>
+<rect x="163.582" y="21.6493" width="100" height="210.566" rx="30" transform="rotate(34.2964 163.582 21.6493)" fill="#8591D7"/>
+</svg>
+
+      <svg className='yellow-ball' width="108" height="104" viewBox="0 0 108 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="58" cy="54" r="49.5" stroke="black"/>
+<circle cx="50" cy="50" r="50" fill="#FCFF53"/>
+</svg>
+<svg className='peach-box' width="137" height="132" viewBox="0 0 137 132" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="9.63689" y="37.2753" width="99" height="99" rx="29.5" transform="rotate(-19.2494 9.63689 37.2753)" stroke="black"/>
+<rect y="32.9681" width="100" height="100" rx="30" transform="rotate(-19.2494 0 32.9681)" fill="#FD8C69"/>
+</svg>
+
     <h3>Forged from evolution, neuroscience, and math,
        our mission is to elevate human potential. Beyond
         products, we're shaping a future where every family
--- src/Components/Product/Func/Limited/Limited.js
+++ src/Components/Product/Func/Limited/Limited.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Limited.css"
+
 function Limited() {
   return (
     <div className='Limited'>
