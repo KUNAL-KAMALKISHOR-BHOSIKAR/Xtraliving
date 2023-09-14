9e932c74734b4773bcacc24540e975bf8fb5e484
adnan shaikh <adnn4u@gmail.com>
2023-09-14 13:05:58 +0530

    takeaway points

--- src/Components/Workshop/Takeaways/Takeaways.js
+++ src/Components/Workshop/Takeaways/Takeaways.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import "./Takeaways.css"
 import way from "./img/way.png"
-import point from "./img/point.png"
+import t1 from "./img/t1.png"
+import t2 from "./img/t2.png"
+import t3 from "./img/t3.png"
+import t4 from "./img/t4.png"
+
 import background from "./img/background.png"
 function Takeaways() {
   return (
@@ -17,18 +21,31 @@ function Takeaways() {
         </div>
         <div className="way-text">
     <div className="point">
-        <img src={point} alt="" height={400} />
-<div className="way-img-text">
-  <p>Time-efficient exercises for strength <br/> and stamina.</p>
-  <p>Nutritional hacks for sustained energy.</p>
-  <p>Stress-management techniques<br/> tailored for the boardroom.</p>
-  <p>Sleep strategies for restorative nights.</p>
-</div>
+       <div className="point-item">
+        <img src={t1} alt="" />
+       <p>Time-efficient exercises for strength and stamina.</p>
+       </div>
+       <div className="point-item">
+       <img src={t2} alt="" />
+
+       <p>Nutritional hacks for sustained energy.</p>
+       </div>
+       <div className="point-item">
+       <img src={t3} alt="" />
+
+       <p>Stress-management techniques tailored for the boardroom.</p>
+       </div>
+       <div className="point-item">
+       <img src={t4} alt="" />
+
+       <p>Sleep strategies for restorative nights.</p>
+       </div>
     </div>
         </div>
         </div>  
     </div>
   )
 }
+
 
 export default Takeaways
