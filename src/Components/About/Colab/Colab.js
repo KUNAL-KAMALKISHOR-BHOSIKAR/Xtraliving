a8c04d2e6507024921c017d907b12848a99e2064
Adnan1O <adnn4u@gmail.com>
2023-09-15 14:34:20 +0530

    forms added

--- src/Components/About/Colab/Colab.js
+++ src/Components/About/Colab/Colab.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Colab.css"
 import colab from "./img/colab.png"
+import c1 from "./img/c1.png"
+import c2 from "./img/c2.png"
+import c3 from "./img/c3.png"
 import line from "./img/line.png"
 import largecolab from "./img/largecolab.png"
 function Colab() {
@@ -14,13 +17,15 @@ function Colab() {
          to improve human performance in India</h1>
          <div className="large">
        <div className="first-p">
-        <p>Partnered with Gopichand Academy
+        <p> <img src={c1} alt="" />Partnered with Gopichand Academy
           to improve performance of athletes</p>
        </div>
        <img src={largecolab} alt="" />
        <div className="second-p">
-        <p>Exclusive tie-up for India with global
+        <p><img src={c2} alt="" />Exclusive tie-up for India with global
 leaders Compex and Precision Nutrition</p>
+<p> <img src={c3} alt="" /> Exclusive tie-up for India with global leaders
+   Compex and Precision Nutrition</p>
        </div>
            </div>
          <img src={colab} alt="" className='forsmall' />
