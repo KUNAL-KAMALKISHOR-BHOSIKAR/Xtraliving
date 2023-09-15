3b778c6bf8b3d93e4489b4c21d2de8ccd11f8c6c
Adnan1O <adnn4u@gmail.com>
2023-09-15 19:17:29 +0530

    forms added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,31 +5,36 @@ import { AiFillLinkedin } from 'react-icons/ai';
 import { AiFillInstagram } from 'react-icons/ai';
 import { AiFillTwitterSquare } from 'react-icons/ai';
 import { AiFillFacebook } from 'react-icons/ai';
+import { Link } from "react-router-dom";
 const Footer=()=>{
 return(
     <div className="Footer">
         <div className="part-1">
-    <img src={logo} height={25} alt="" />
+   <Link to="/"> <img src={logo} height={25} alt="" /></Link>
     <div className="list">
     <div  className="list1"> 
     <ul>
-        <li>About us</li>
+      <Link to="/about"><li>About us</li></Link>
         <li>Products</li>
-        <li>FUNc Community</li>
+      <Link to="/func">  <li>FUNc Community</li></Link>
     </ul>
     </div>
 
     <div  className="list1"> 
     <ul>
-        <li>Media</li>
-        <li>latest videos</li>
-        <li>Articles</li>
+     <a target="_blank" href="https://www.rishi.fit/media">
+          <li>Media</li></a> 
+       <a target="_blank" href="https://www.youtube.com/@rishi9566">
+         <li>latest videos</li></a>
+       <a target="_blank"  href="https://www.rishi.fit/myBlogs">
+         <li>Articles</li></a>
     </ul>
     </div>
 
     <div  className="list1"> 
     <ul>
-        <li>Contact</li>
+       <a target="_blank" href="https://forms.myfunc.in/myfunc/form/ConnectwithRishi/formperma/HkPge_e3v8bQoOpsQka6qPmQ5YBTC29DAGd2H5FJaHk">
+       <li>Contact</li> </a> 
         <li>Privacy policy</li>
      
     </ul>
