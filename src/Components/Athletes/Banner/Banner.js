b8bc77ceb79f67ecab82780c4e58c02187e30d9b
Adnan1O <adnn4u@gmail.com>
2023-09-18 11:57:09 +0530

    func page

--- src/Components/Athletes/Banner/Banner.js
+++ src/Components/Athletes/Banner/Banner.js
@@ -1,24 +1,24 @@
 import React from 'react'
-
+import "./Ath.css"
 import banner from "./img/banner.png"
 import { BsArrowRight } from 'react-icons/bs';
 function Banner() {
   return (
-    <div className='banner'>
+    <div className='banner-ath'>
   
-     <div className="hero">
-       <div className="hero-text ath-txt">
+     <div className="ath-hero">
+       <div className="ath-hero-text">
 <h1>Unleash Your Olympic Potential</h1>
 
 <p>Master Movement & Nutrition with <br/>Elite Training Regimens</p>
 <a href="https://forms.myfunc.in/myfunc/form/UnlockElitePerformanceforLeaders/formperma/4b4maZKQlhbNpgMtSqCH_nYsizcB0Ad5yxk4IXoMIhA">
-  <button>Enroll today <BsArrowRight/></button></a>
+  <button>Enroll today <BsArrowRight/></button>
+  </a>
        </div>
-       <div className="hero-img ath-banner">
-    <img src={banner} alt=""  className='ath-img'  />
+       <div className="ath-hero-img">
+    <img src={banner} alt="" />
        </div>
      </div>
-    
     </div>
   )
 }
