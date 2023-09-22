75edf2adbc9735131860c68ccb9b8215ae5f60a8
Adnan1O <adnn4u@gmail.com>
2023-09-22 15:17:45 +0530

    more media pages added

--- src/Components/Media/Previous/Previous.js
+++ src/Components/Media/Previous/Previous.js
@@ -62,7 +62,82 @@ function Previous() {
         </div>
          </Link>
       </div>
+    <div className="newspaper">
+      <h2>Media</h2>
+      <div className="artical-area">
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-1.1.412e4c4faa14ac0eda76.png" alt="" />
+      <h3>India Today</h3>
+      <p>India Today features Rishikesh Kumar's authored article on Improving Children's Health amidst the pandemic</p>
     </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-2.1.bc964725129523cfbf3b.png" alt="" />
+      <h3>Times of India</h3>
+      <p>eNERGIZE - National Level Online Athletic Fitness Championship</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-3.1.7bfd3e18d3898c9c2732.jpg" alt="" />
+      <h3>The New Indian Express</h3>
+      <p>Work-out from home is the new normal</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-4.1.447dfd9ffd80dae2375f.png" alt="" />
+      <h3>Money Control</h3>
+      <p>Technology provoking Indians to stay fit amid Covid-19 lockdown</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-5.1.ddd45ea0a430c1d808a0.png" alt="" />
+      <h3>ET Healthworld</h3>
+      <p>India Today features Rishikesh Kumar's authored article on Improving Children's Health amidst the pandemic</p>
+    </div>
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-6.1.e302298914a746856738.png" alt="" />
+      <h3>Newsd</h3>
+      <p>National Level Online Fitness Athletic Championship expected to witness more than 1,00,000 participants</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-7.1.8cf53106832206d3eec1.png" alt="" />
+      <h3>Jhalak</h3>
+      <p>News coverage dedicated to eNERGIZE</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-8.1.caf9703666b4ddd9409d.png" alt="" />
+      <h3>News Patrolling</h3>
+      <p>Xtraliving, Sports Authority of India and Directorate of Sports and Youth services of Maharashtra join hands to host eNERGIZE</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-9.1.b86ff4f2aadd0bdf0a5e.png" alt="" />
+      <h3>IANS Life</h3>
+      <p>Xtraliving all set for National Level Online Fitness Athletic Championship</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-10.1.71653c751f55c757756d.png" alt="" />
+      <h3>Telangana Today</h3>
+      <p>X60 Crossfit in Headlines</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-11.1.1961c02bce1a05eb446c.png" alt="" />
+      <h3>Healthwire</h3>
+      <p>Boosting your immune system</p>
+    </div>
+
+    <div className="artical">
+      <img src="https://www.rishi.fit/static/media/media-12.1.2cf3ab68f6a7897f5e8b.png" alt="" />
+      <h3>News Karnataka</h3>
+      <p>Hunt for 'fastest girl in Maharashtra' begins</p>
+    </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
